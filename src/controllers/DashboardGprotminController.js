f86f5301f25fda6aef224f0a96ec246b69c441d3
Glaysonvisgueira <glaysonwow@gmail.com>
2021-09-22 08:39:23 -0300

    Add sort method in controller

--- src/controllers/DashboardGprotminController.js
+++ src/controllers/DashboardGprotminController.js
@@ -2,7 +2,7 @@ const DashboardGprotmin = require('../models/DashboardGprotmin');
 
 module.exports = {
     async index(request, response){
-        const dashboards = await DashboardGprotmin.find(); 
+        const dashboards = await DashboardGprotmin.find().sort({"dados_dashboard": -1});
         return response.json(dashboards);
     }
 }
