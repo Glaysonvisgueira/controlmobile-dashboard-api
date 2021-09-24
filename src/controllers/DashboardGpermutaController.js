cf9e44292375f6e307d6b0216d8a8b466914b647
Glaysonvisgueira <glaysonwow@gmail.com>
2021-09-24 13:38:40 -0300

    Add new sort

--- src/controllers/DashboardGpermutaController.js
+++ src/controllers/DashboardGpermutaController.js
@@ -2,7 +2,7 @@ const DashboardGpermuta = require('../models/DashboardGpermuta');
 
 module.exports = {
     async index(request, response){
-        const dashboards = await DashboardGpermuta.find().sort({"gpermuta.dados_dashboard": -1});
+        const dashboards = await DashboardGpermuta.find().sort({"gpermuta.dados_dashboard": -1, "sigla_dep": 1});
         return response.json(dashboards);
     }
 }
